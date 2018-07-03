de3fc316009380b23d76ca46ccc3a9f409e511be
Ерболат Бейсенбек <hg1zadr@gmail.com>
2018-07-03 09:37:39 +0600

    Added Assets

---
+++ Assets/Project/Scripts/SimpleParallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleParallax : MonoBehaviour {
+
+	public GameObject parallaxTarget;
+	public float tileHeight;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < transform.childCount; i++) {
+			GameObject currentTile = transform.GetChild(i).gameObject;
+			if(parallaxTarget.transform.y - currentTile.transform.position.y >= tileHeight){
+				currentTile.transform.position = new Vector2(
+				0,
+				currentTile.transform.position.y + transform.childCount * tileHeight
+				);
+			}
+		}
+	}
+}
